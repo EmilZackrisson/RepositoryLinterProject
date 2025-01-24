60b89336df3cfdf49f1a24417937431dc2c796c1
Theo Svensson <theosvensson5@gmail.com>
2025-01-24 09:22:14 +0100

    FIrst

---
+++ Bomb.cs
@@ -0,0 +1,6 @@
+class BOMB{
+private int value = 3;
+public string printValue() {
+    return this.value.ToString();
+}
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using System.CommandLine;
+
+// var files = Directory.EnumerateFiles();
+
+var bomb = new BOMB();
+Console.WriteLine(bomb.printValue());
