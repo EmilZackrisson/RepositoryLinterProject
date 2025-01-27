1825ccaabed314851c8e43be2a3da7a89d18bea5
Theo Svensson <theosvensson5@gmail.com>
2025-01-27 19:03:00 +0100

    w

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 namespace app;
 
+/*
 class Program
 {
     static async Task<int> Main(string[] args)
@@ -9,6 +10,34 @@ class Program
         var fileOption = new Option<FileInfo?>(
             name: "--file",
             description: "The file to read and display on the console.");
+
+        var rootCommand = new RootCommand("Sample app for System.CommandLine");
+        rootCommand.AddOption(fileOption);
+
+        rootCommand.SetHandler((file) => 
+            { 
+                ReadFile(file!); 
+            },
+            fileOption);
+
+        return await rootCommand.InvokeAsync(args);
+    }
+
+    static void ReadFile(FileInfo file)
+    {
+        File.ReadLines(file.FullName).ToList()
+            .ForEach(line => Console.WriteLine(line));
+    }
+}
+*/
+
+class Program
+{
+    static async Task<int> Main(string[] args)
+    {
+        var urlOption = new Option<Uri>(
+            name: "--url",
+            description: "The url to read and from which a GitHub repository should be cloned.");
 
         var rootCommand = new RootCommand("Sample app for System.CommandLine");
         rootCommand.AddOption(fileOption);
