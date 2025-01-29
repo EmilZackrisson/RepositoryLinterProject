958d99fdf840f48c9abe5fe515f7bd54224158a6
Theo Svensson <theosvensson5@gmail.com>
2025-01-29 10:31:57 +0100

    Cleaner System.CommandLine + url validation

--- Program.cs
+++ Program.cs
@@ -6,22 +6,46 @@ class Program
 {
     static async Task<int> Main(string[] args)
     {
-        var urlOption = new Option<string?>(
-            name: "--url",
-            description: "The URL to take and validate."
-        );
+        var rootCommand = new RootCommand("A simple program that takes a URL or a path and validates it.");
 
-        var pathOption = new Option<string?>(
-            name: "--path",
-            description: "The path to take and validate."
-        );
+        var urlCommand = new Command("url", "Run linter on URL");
+        var urlArg = new Argument<string>("url", "URL");
 
-        var rootCommand = new RootCommand("A simple program that takes a URL or a path and validates it.")
-        {
-            urlOption,
-            pathOption
-        };
+        urlArg.AddValidator((url) => {
 
+            if (!URLValidator.IsValidURL(url.Tokens[0].Value)){
+                url.ErrorMessage = $"Invalid URL: {url.Tokens[0].Value}";
+            }
+        });
+
+        urlCommand.AddArgument(urlArg);
+
+        var pathCommand = new Command("path", "Run linter on path");
+        var pathArg = new Argument<string>("path", "Path");
+
+        pathArg.AddValidator((path) => {
+
+            if (true){
+                path.ErrorMessage = $"Invalid URL: {url.Tokens[0].Value}";
+            }
+        });
+
+        pathCommand.AddArgument(pathArg);
+
+        rootCommand.AddCommand(urlCommand);
+        rootCommand.AddCommand(pathCommand);
+
+        urlCommand.SetHandler((url) => {
+            Console.WriteLine($"You entered URL: {url}");
+                Console.WriteLine(URLValidator.IsValidURL(url));
+        }, urlArg);
+
+        
+
+        pathCommand.SetHandler((path)=> {
+            Console.WriteLine($"You entered path: {path}");
+        }, pathArg);
+        /*
         rootCommand.SetHandler((string? url, string? path) =>
         {
             if (url != null)
@@ -40,7 +64,7 @@ class Program
             {
                 Console.WriteLine("Please provide either a URL or a path.");
             }
-        }, urlOption, pathOption);
+        }, urlOption, pathOption);*/
 
         return await rootCommand.InvokeAsync(args);
     }
